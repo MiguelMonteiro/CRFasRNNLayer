49a01b9f74bafb5d58b91066319aaf68eae26e84
MiguelMonteiro <mab.mtr@gmail.com>
2019-01-08 14:54:21 +0000

    build refactor, builds fine in python3

--- build.sh
+++ build.sh
@@ -1,34 +1,10 @@
 #!/usr/bin/env bash
 
+rm lattice_filter.so
 rm lattice_filter_op_loader.py
+cd permutohedral_lattice
+sh build.sh
+cp lattice_filter.so ../
+cp lattice_filter_op_loader.py ../
 rm lattice_filter.so
 
-mkdir build_dir
-cd build_dir
-
-
-CUDA_COMPILER=/usr/local/cuda/bin/nvcc
-CXX_COMPILER=/usr/bin/g++-6
-
-SPATIAL_DIMS=2
-INPUT_CHANNELS=3
-REFERENCE_CHANNELS=3
-MAKE_TESTS=False
-
-cmake -DCMAKE_BUILD_TYPE=Debug -D CMAKE_CUDA_COMPILER=${CUDA_COMPILER} \
-                               -D CMAKE_CXX_COMPILER=${CXX_COMPILER} \
-                               -D CMAKE_CUDA_HOST_COMPILER=${CXX_COMPILER} \
-                               -D SPATIAL_DIMS=${SPATIAL_DIMS} \
-                               -D INPUT_CHANNELS=${INPUT_CHANNELS} \
-                               -D REFERENCE_CHANNELS=${REFERENCE_CHANNELS} \
-                               -D MAKE_TESTS=${MAKE_TESTS} \
-                               -G "CodeBlocks - Unix Makefiles" ../
-
-make
-
-
-cp ../permutohedral_lattice/lattice_filter_op_loader.py ../
-cp lattice_filter.so ../
-
-cd ..
-rm -r build_dir
